5d05b1b265c9b74cabca53685aa21ea435bdbb89
Sibtain Raza <khanbhai14063@gmail.com>
2026-04-06 23:00:07 +0530

    Done TodoForm change

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,17 +2,7 @@ import React, { useState } from 'react'
 import {useTodo} from '../context/TodoContext'
 
 const TodoForm = () => {
-  const [todo, setTodo] = useState("")
-  const {addTodo} = useTodo()
-
-  const add = (e)=>{
-    e.preventDefault();
-
-    if(!todo) return
-
-    addTodo({todo, completed: false})
-    setTodo("")
-  }
+  
   return (
     <form onSubmit={add} className='flex'>
       <input type="text"
